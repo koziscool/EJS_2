94e34a2e84125c1247f111605328413dd2d3319d
koziscool <kozjob@yahoo.com>
2017-02-14 16:42:07 -0600

    again

--- Ch2.js
+++ Ch2.js
@@ -9,4 +9,32 @@ var poundTriangle = function( limit ) {
   }
 }
 
-poundTriangle(7);
+var fizzBuzz = function () {
+  for( var i = 1; i <= 100; i++) {
+    if (i % 3 !== 0 && i % 5 !== 0)
+      console.log(i);
+    if (i % 3 === 0 && i % 5 !== 0)
+      console.log("Fizz");
+    if (i % 3 !== 0 && i % 5 === 0)
+      console.log("Buzz");
+    if (i % 3 === 0 && i % 5 === 0)
+      console.log("FizzBuzz");
+  }
+}
+
+var chessBoard = function( limit) {
+  for( var i = 0; i < limit; i++) {
+    var s = ""
+    for( var j= 0; j < limit; j++) {
+      if( (i+j) % 2 === 0 )
+        s += " ";
+      else
+        s += "#";
+    }
+    console.log(s);
+  }
+}
+
+// poundTriangle(7);
+// fizzBuzz();
+chessBoard(8);
