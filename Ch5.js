944b7542bb19346f5d0689b9d5957b278b593f28
koziscool <kozjob@yahoo.com>
2017-02-14 23:12:51 -0600

    again

--- Ch5.js
+++ Ch5.js
@@ -80,6 +80,5 @@ var some = function( arr, test ) {
 
 console.log(every([NaN, NaN, NaN], isNaN));
 console.log(every([NaN, NaN, 4], isNaN));
-
 console.log(some([NaN, 3, 4], isNaN));
 console.log(some([2, 3, 4], isNaN));
